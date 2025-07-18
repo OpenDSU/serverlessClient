f491b28650fd482eff0bbef7a8af1540274131a3
skutner <sava.dumitru.daniel@gmail.com>
2025-07-18 15:40:46 +0300

    Adds webhookUrl parameter
    Adds `webhookUrl` parameter to the `createServerlessAPIClient` function.
    
    This allows clients to specify a webhook URL when creating a
    Serverless API client.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const ServerlessClient = require('./ServerlessClient');
 const PendingCallMixin = require('./utils/PendingCallMixin');
 const getBaseURL = require('./utils/getBaseURL');
 
-async function createServerlessAPIClient(userId, endpoint, serverlessId, pluginName, options) {
+async function createServerlessAPIClient(userId, endpoint, serverlessId, pluginName, webhookUrl, options) {
     const client = new ServerlessClient(userId, endpoint, serverlessId, pluginName, options);
     return await client.init();
 }
