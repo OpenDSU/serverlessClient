343c1c22f093289968fc8568c9f4d951bd057aa4
skutner <sava.dumitru.daniel@gmail.com>
2025-08-22 11:10:56 +0300

    Configures library for browser compatibility
    Adds necessary plugins to ensure the library functions correctly in a browser environment.
    
    These changes configure Rollup to resolve Node.js modules and handle CommonJS modules, enabling the library to be used in browser-based applications.

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,32 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'index.js'),
       name: 'ServerlessClient',
-      fileName: (format) => `serverless-client.${format === 'es' ? 'mjs' : 'umd.js'}`
+      fileName: (format) => `serverless-client.${format === 'es' ? 'mjs' : 'umd.js'}`,
+      formats: ['es', 'umd']
     },
     rollupOptions: {
       external: [],
+      plugins: [
+        nodeResolve({
+          preferBuiltins: false,
+          browser: true
+        }),
+        commonjs({
+          transformMixedEsModules: true
+        })
+      ],
       output: {
-        globals: {}
+        globals: {},
+        exports: 'named'
       }
-    }
+    },
+    minify: false
   }
 });
